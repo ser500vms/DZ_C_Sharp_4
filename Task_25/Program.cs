e1a5ff4005c476aac4fe0de9cdc0aaf7ed8336f6
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-15 19:14:56 +0300

    Исправил решение в задании №25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -5,35 +5,10 @@
 
 // Решение через Math.Pow:
 
-// int GetPower(int num1, int num2)
-// {
-//     double power = 0;
-//     power = Math.Pow(num1, num2);
-//     return Convert.ToInt32(power);
-// }
-
-// int GetInput(string text)
-// {
-//     Console.Write(text);
-//     return Convert.ToInt32(Console.ReadLine());
-// }
-
-// int number1 = GetInput("Введите 1-е число: ");
-// int number2 = GetInput("Введите 2-е число: ");
-// Console.Write($"{number1}, {number2} -> {GetPower(number1, number2)}");
-
-// Решение через цикл:
-
 int GetPower(int num1, int num2)
 {
-    int power = 1;
-    int count = 0;
-    while (count < num2)
-    {
-        power = power * num1;
-        count++;
-    }
-    return power;
+    double power = Math.Pow(num1, num2);
+    return Convert.ToInt32(power);
 }
 
 int GetInput(string text)
@@ -45,4 +20,28 @@ int GetInput(string text)
 int number1 = GetInput("Введите 1-е число: ");
 int number2 = GetInput("Введите 2-е число: ");
 Console.Write($"{number1}, {number2} -> {GetPower(number1, number2)}");
+
+// Решение через цикл:
+
+// int GetPower(int num1, int num2)
+// {
+//     int power = 1;
+//     int count = 0;
+//     while (count < num2)
+//     {
+//         power = power * num1;
+//         count++;
+//     }
+//     return power;
+// }
+
+// int GetInput(string text)
+// {
+//     Console.Write(text);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int number1 = GetInput("Введите 1-е число: ");
+// int number2 = GetInput("Введите 2-е число: ");
+// Console.Write($"{number1}, {number2} -> {GetPower(number1, number2)}");
 
